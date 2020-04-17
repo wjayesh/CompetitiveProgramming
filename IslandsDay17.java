e80445badccb8d459e1c1c66fbd2e0598e8e15b0
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-04-18 00:17:22 +0530

    Refined, Day 17

--- IslandsDay17.java
+++ IslandsDay17.java
@@ -1,33 +1,39 @@
 
 public class IslandsDay17 {
-    public static int numIslands(char[][] grid) {
-        int count = 0;
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
-                if(grid[i][j] == '1' && noNearbyIsland(grid, i, j)) {
-                    grid[i][j] = '2';
+    public int numIslands(char[][] grid) {
+        if(grid==null || grid.length==0||grid[0].length==0)
+            return 0;
+     
+        int m = grid.length;
+        int n = grid[0].length;
+     
+        int count=0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]=='1'){
                     count++;
+                    merge(grid, i, j);
                 }
             }
         }
+     
         return count;
     }
-
-    private static boolean noNearbyIsland(char[][] grid, int i, int j) {
-        int up = Math.max(i - 1, 0); int down = Math.min(i + 1, grid.length - 1);
-        int left = Math.max(j - 1, 0); int right = Math.min(j + 1, grid[0].length - 1);
-
-        if(grid[up][j] == '2' || grid[down][j] == '2' 
-            || grid[i][left] == '2' || grid[i][right] == '2') {
-            grid[i][j] = '2';
-            return false;
-        }
-        return true;
+     
+    public void merge(char[][] grid, int i, int j){
+        int m=grid.length;
+        int n=grid[0].length;
+     
+        if(i<0||i>=m||j<0||j>=n||grid[i][j]!='1')
+            return;
+     
+        grid[i][j]='X';
+     
+        merge(grid, i-1, j);
+        merge(grid, i+1, j);
+        merge(grid, i, j-1);
+        merge(grid, i, j+1);
     }
 
-    public static void main(String[] args) {
-        char[][] grid = new char[][]{{'1','1','1','1','0'},{'1','1','0','1','0'},{'1','1','0','0','0'},{'0','0','0','0','0'}};
-        System.out.println(numIslands(grid));
-    }
 
 }
