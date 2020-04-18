8160db5ce38d9f64271bb8331377cc6855e6a02c
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-04-18 23:32:53 +0530

    Bottoms up DP

--- MinPathSumDay18.java
+++ MinPathSumDay18.java
@@ -1,26 +1,22 @@
 
 public class MinPathSumDay18 {
     public int minPathSum(int[][] grid) {
-        return minPathSumDP(grid, 0, 0);
-    }
+        int[][] sum = new int[grid.length][grid[0].length];
+        sum[0][0] = grid[0][0];
 
-    private int minPathSumDP(int[][] grid, int i, int j) {
-        if(i == grid.length - 1) {
-            int sum = 0;
-            while(j != grid[0].length) {
-                sum += grid[i][j];
-                j++;
-            }
-            return sum;
+        for(int i = 1; i < grid.length; i++) {
+            sum[i][0] = sum[i-1][0] + grid[i][0];
         }
-        if(j == grid[0].length - 1) {
-            int sum = 0;
-            while(i != grid.length) {
-                sum += grid[i][j];
-                i++;
-            }
-            return sum;
+        for(int i = 1; i < grid[0].length; i++) {
+            sum[0][i] = sum[0][i-1] + grid[0][i];
         }
-        return grid[i][j] + Math.min(minPathSumDP(grid, i, j+1), minPathSumDP(grid, i+1, j));
+
+        for(int i = 1; i < grid.length; i++) {
+            for(int j = 1; j < grid[0].length; j++) {
+                sum[i][j] = grid[i][j] + Math.min(sum[i-1][j], sum[i][j-1]);
+            }
+        }
+
+        return sum[grid.length-1][grid[0].length-1];
     }
 }
