0f72a8626d04d8e4e9feb6a6de71f5bcf8032a2c
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-04-28 16:26:45 +0530

    lcs dp

--- LCSubsequence.java
+++ LCSubsequence.java
@@ -1,16 +1,10 @@
 class LCSubsequence {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int i = 0, j = 0;
-        int count = 0;
-        
-        while(i < text1.length() && j < text2.length()) {
-            if(text1.charAt(i) == text2.charAt(j)) {
-                j++; i++;
-                count++;
-            } else {
-                i++;
-            }
-        }
-        return count;
+    public int longestCommonSubsequence(String word1, String word2) {
+        int dp[][] = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 1; i <= word1.length(); i++)
+            for (int j = 1; j <= word2.length(); j++)
+                dp[i][j] = word1.charAt(i - 1) == word2.charAt(j - 1) ? dp[i - 1][j - 1] + 1
+                        : Math.max(dp[i - 1][j], dp[i][j - 1]);
+        return dp[word1.length()][word2.length()];
     }
 }
