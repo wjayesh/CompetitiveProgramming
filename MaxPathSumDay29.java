b6290defb6b04dfd3c3105564198f8fa4ce07607
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-04-30 23:35:06 +0530

    fixed by adding global max

--- MaxPathSumDay29.java
+++ MaxPathSumDay29.java
@@ -2,19 +2,26 @@ import utils.TreeNode;
 
 public class MaxPathSumDay29 {
     public int maxPathSum(TreeNode root) {
-        return maxSum(root);
+        GlobalMax globalMax = new GlobalMax();
+        globalMax.val = Integer.MIN_VALUE;
+        maxSum(root, globalMax);
+        return globalMax.val;
     }
 
-    private int maxSum(TreeNode root) {
+    private int maxSum(TreeNode root, GlobalMax obj) {
         if(root.left == null || root.right == null) {
             return root.val;
         }
         int sum = 0;
-        int leftSum = maxSum(root.left);
-        int rightSum = maxSum(root.right);
+        int leftSum = maxSum(root.left, obj);
+        int rightSum = maxSum(root.right, obj);
         sum = Math.max(leftSum + rightSum + root.val, root.val);   
         sum = Math.max(sum, Math.max(leftSum, rightSum) + root.val);
+        obj.val = Math.max(obj.val, sum);
         return sum;
     }
 
+}
+class GlobalMax {
+    int val;
 }
