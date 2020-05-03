96d7ffd8cb82117701cf86f5d9b624fd33c5b054
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-05-03 22:28:28 +0530

    streams :fist

---
+++ Jewels2.java
@@ -0,0 +1,8 @@
+
+public class Jewels2 {
+    public int numJewelsInStones(String J, String S) {
+        int identify[] = new int[128];
+        J.chars().forEach(c -> identify[c] = 1);
+        return (int)S.chars().filter(c -> identify[c] == 1).count();
+    }
+}
