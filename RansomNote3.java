478fb2f16255a2224fd2bfefbb6197e8720f4ab9
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-05-03 22:15:45 +0530

    elegant 3-liner

--- RansomNote3.java
+++ RansomNote3.java
@@ -1,32 +1,10 @@
-import java.util.HashMap;
-import java.util.Map;
+
 
 class RansomNote3 {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> map = new HashMap<>();
-        preprocess(magazine, map);
-        for(int i = 0; i < ransomNote.length(); i++) {
-            char c = ransomNote.charAt(i);
-            if(!map.containsKey(c)) {
-                return false;
-            } else if (map.get(c) == 0) {
-                return false;
-            } else {
-                map.put(c, map.get(c) - 1);
-            }
-        }
-        return true;
-    }
-
-    private void preprocess(String magazine, Map<Character, Integer> map) {
-        for(int i = 0; i < magazine.length(); i++) {
-            char c = magazine.charAt(i);
-            if(map.containsKey(c)) {
-                map.put(c, map.get(c) + 1);
-            } else {
-                map.put(c, 1);
-            }
-        }
+        int[] counts = new int[128];
+        magazine.chars().forEach(c -> counts[c]++);
+        return ransomNote.chars().allMatch(c -> counts[c]-- > 0);
     }
 
     public static void main(String[] args) {
