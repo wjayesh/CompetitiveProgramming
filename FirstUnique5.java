41cc13dad2d7ba3955c9789e332b1eca9521d130
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-05-05 23:10:47 +0530

    simple array based

---
+++ FirstUnique5.java
@@ -0,0 +1,12 @@
+public class FirstUnique5 {
+    public int firstUniqChar(String s) {
+        char letters[] = new char[128];
+        s.chars().forEach(c -> letters[c]++);
+        for(int i = 0; i < s.length(); i++) {
+            if(letters[s.charAt(i)] == 1) {
+                return i;
+            }
+        }
+        return -1;       
+    }
+}
