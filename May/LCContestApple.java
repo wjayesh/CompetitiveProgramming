583cd4d58850c1c3358c343ee36fa54341b8ce30
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-05-11 16:55:04 +0530

    if no apple below, subtract from sum

---
+++ May/LCContestApple.java
@@ -0,0 +1,47 @@
+package May;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+public class LCContestApple {
+    HashMap<Integer, Boolean> map = new HashMap<>();
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        int sum = edges.length * 2;
+        for(int i = 0; i < n; i++) {
+            if(noAppleBelow(i, edges, hasApple)) {
+                sum -= 2;
+            }
+        }
+        return sum;
+    }
+    
+    private boolean noAppleBelow(int n, int[][] edges, List<Boolean> hasApple) {
+        if(map.containsKey(n)) {
+            return map.get(n);
+        }
+        if(!hasApple.get(n)) {
+            for(int i = 0; i < edges.length; i++) {
+                if(edges[i][0] == n) {
+                    if(!noAppleBelow(edges[i][1], edges, hasApple)) {
+                        map.put(i, false);
+                        return false;    
+                    }
+                } 
+            }
+        } else {
+            map.put(n, false);
+            return false;
+        }
+        map.put(n, true);
+        return true;
+    }
+    public static void main(String[] args) {
+        LCContestApple obj = new LCContestApple();
+        ArrayList<Boolean> list = new ArrayList<Boolean>();
+        list.add(false); list.add(true);
+        obj.minTime(2, new int[][]{{1, 2}}, list);
+    }
+}
+    
