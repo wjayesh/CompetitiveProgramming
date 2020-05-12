e132661a08cd501ef387cd4592ea800f16d5e4fd
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-05-12 22:34:10 +0530

    added log N soln

--- May/SingleNumber12.java
+++ May/SingleNumber12.java
@@ -16,4 +16,25 @@ public class SingleNumber12 {
         }
         return sum;
     }
+
+    /**
+     * O(log N) approach
+     * 
+     * 
+     *  public int singleNonDuplicate(int[] nums) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo < hi) {
+            int mid = (lo + hi) >>> 1;
+            if (nums[mid] == nums[mid ^ 1])
+                lo = mid + 1;
+            else
+                hi = mid;
+            }
+        return nums[lo];
+        }
+
+        Note:
+        odd xor 1 = odd-1
+        even xor 1 = even+1
+     */
 }
