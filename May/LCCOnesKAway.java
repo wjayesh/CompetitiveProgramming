fe29e1bc9c70142b805b88e46a6d640563a4a548
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-05-13 15:40:11 +0530

    fix for all zeroes

--- May/LCCOnesKAway.java
+++ May/LCCOnesKAway.java
@@ -3,7 +3,7 @@ package May;
 public class LCCOnesKAway {
     public boolean kLengthApart(int[] nums, int k) {
         int i = 0, count = 0;
-        while(nums[i] != 1) {
+        while(i < nums.length && nums[i] != 1) {
             i++;
         }
         for(int j = i + 1; j < nums.length; j++) {
