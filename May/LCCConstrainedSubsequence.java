d0acd19109a750b21b4e8f5c5377f6b07c720a93
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-05-17 15:22:21 +0530

    using monoqueue

---
+++ May/LCCConstrainedSubsequence.java
@@ -0,0 +1,22 @@
+package May;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LCCConstrainedSubsequence {
+    public int constrainedSubsetSum(int[] A, int k) {
+        int res = A[0];
+        Deque<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < A.length; ++i) {
+            A[i] += !q.isEmpty() ? q.peek() : 0;
+            res = Math.max(res, A[i]);
+            while (!q.isEmpty() && A[i] > q.peekLast())
+                q.pollLast();
+            if (A[i] > 0)
+                q.offer(A[i]);
+            if (i >= k && !q.isEmpty() && q.peek() == A[i - k])
+                q.poll();
+        }
+        return res;
+    }
+}
