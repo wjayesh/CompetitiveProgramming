d52e8cfcd407c49cd2156bdd4c2b523268c779f7
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-05-19 15:31:07 +0530

    using maps, sets and lists

---
+++ May/LCCDisplayFoodOrders.java
@@ -0,0 +1,37 @@
+package May;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class LCCDisplayFoodOrders {
+    public List<List<String>> displayTable(List<List<String>> orders) {
+        final Map<Integer, Map<String, Integer>> map = new HashMap<>();
+        final Set<String> dishes = new HashSet<>();
+        for (List<String> o : orders) {
+            map.computeIfAbsent(Integer.parseInt(o.get(1)), v -> new HashMap<>())
+               .merge(o.get(2), 1, Integer::sum);
+            dishes.add(o.get(2));
+        }
+        final List<String> sortedDish = new ArrayList<>(dishes);
+        final List<Integer> tables = new ArrayList<>(map.keySet());
+        Collections.sort(sortedDish);
+        Collections.sort(tables);
+        final List<List<String>> res = new ArrayList<>();
+        for (Integer table : tables) {
+            final List<String> append = new ArrayList<>(Collections.singleton(String.valueOf(table)));
+            final Map<String, Integer> currTable = map.get(table);
+            for (String s : sortedDish) {
+                append.add(String.valueOf(currTable.getOrDefault(s, 0)));
+            }
+            res.add(append);
+        }
+        sortedDish.add(0, "Table");
+        res.add(0, sortedDish);
+        return res;
+    }
+}
