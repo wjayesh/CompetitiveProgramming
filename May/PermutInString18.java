01548ee28f316d2823f4dfaba8f00598b7efc4bc
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-05-19 10:28:37 +0530

    same logic as day 17

---
+++ May/PermutInString18.java
@@ -0,0 +1,42 @@
+package May;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PermutInString18 {
+    public boolean checkInclusion(String t, String s) {
+        if(t.length()> s.length()) return false;
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int counter = map.size();
+        
+        int begin = 0, end = 0;
+        
+        while(end < s.length()){
+            char c = s.charAt(end);
+            if( map.containsKey(c) ){
+                map.put(c, map.get(c)-1);
+                if(map.get(c) == 0) counter--;
+            }
+            end++;
+            
+            while(counter == 0){
+                char tempc = s.charAt(begin);
+                if(map.containsKey(tempc)){
+                    map.put(tempc, map.get(tempc) + 1);
+                    if(map.get(tempc) > 0){
+                        counter++;
+                    }
+                }
+                if(end-begin == t.length()){
+                    return true;
+                }
+                begin++;
+            }
+            
+        }
+        return false;
+    }
+}
