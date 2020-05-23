c2ac34b8b5e786a374294a9d7953226a80ca36cb
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-05-23 23:44:26 +0530

    cleaner, comparing lo and hi

--- May/IntervalIntersections23.java
+++ May/IntervalIntersections23.java
@@ -1,23 +1,29 @@
 package May;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntervalIntersections23 {
     public int[][] intervalIntersection(int[][] A, int[][] B) {
-        int[][] result = new int[A.length + B.length][2];
-        int index = 0, j = 0;
-        for(int i = 0; i < A.length; i++) {
-            int start = A[i][0], end = A[i][1];
-            while(j < B.length && B[j][0] >= start && B[j][0] <= end) {
-                result[index][0] = B[j][0];
-                result[index][1] = Math.min(B[j][1], end);
-                index++; 
-                if(i+1 < A.length && B[j][1] == A[i+1][0]) {
-                    result[index][0] = B[j][1];
-                    result[index][1] = B[j][1];
-                    index++;
-                }
-                j++;
-            }
+        List<int[]> ans = new ArrayList<>();
+        int i = 0, j = 0;
+    
+        while (i < A.length && j < B.length) {
+          // Let's check if A[i] intersects B[j].
+          // lo - the startpoint of the intersection
+          // hi - the endpoint of the intersection
+          int lo = Math.max(A[i][0], B[j][0]);
+          int hi = Math.min(A[i][1], B[j][1]);
+          if (lo <= hi)
+            ans.add(new int[]{lo, hi});
+    
+          // Remove the interval with the smallest endpoint
+          if (A[i][1] < B[j][1])
+            i++;
+          else
+            j++;
         }
-        return result;
-    }
+    
+        return ans.toArray(new int[ans.size()][]);
+      }
 }
