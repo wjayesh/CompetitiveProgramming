280967d573328cb81da4c49620fad9fcd43a8a21
Jayesh Sharma <37150991+wjayesh@users.noreply.github.com>
2020-06-27 15:34:46 +0530

    dp; similar to coin change

--- June/PerfectSquares27.java
+++ June/PerfectSquares27.java
@@ -1,4 +1,23 @@
+
 class PerfectSquares27 {
+    
+    public int numSquares(int n) {
+	int[] dp = new int[n + 1];
+	Arrays.fill(dp, Integer.MAX_VALUE);
+	dp[0] = 0;
+	for(int i = 1; i <= n; ++i) {
+		int min = Integer.MAX_VALUE;
+		int j = 1;
+		while(i - j*j >= 0) {
+			min = Math.min(min, dp[i - j*j] + 1);
+			++j;
+		}
+		dp[i] = min;
+	}		
+	return dp[n];
+    }
+    
+    /**
     public int numSquares(int n) {
         return recurse((double)n);
     }
@@ -13,4 +32,5 @@ class PerfectSquares27 {
         }
         return min + 1;
     }
+    **/
 }
