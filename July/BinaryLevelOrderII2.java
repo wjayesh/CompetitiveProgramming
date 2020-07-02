7328743b1061b2f068de7a0e8a816cb9cf77d9b8
Jayesh Sharma <37150991+wjayesh@users.noreply.github.com>
2020-07-02 23:19:17 +0530

    DFS; create list at index 0 for bottom to top order

---
+++ July/BinaryLevelOrderII2.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BinaryLevelOrderII2 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        levelHelper(res, root, 0);
+        return res;
+    }
+    
+    public void levelHelper(List<List<Integer>> res, TreeNode root, int height) {
+        if (root == null) return;
+        if (height >= res.size()) {
+            res.add(0, new LinkedList<Integer>());
+        }
+        levelHelper(res, root.left, height+1);
+        levelHelper(res, root.right, height+1);
+        res.get(res.size() - height - 1).add(root.val);
+    }
+}
