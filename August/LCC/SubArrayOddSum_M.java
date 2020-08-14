b32a0ab208672dcde9259c6436b6ffdcd28ae67b
Jayesh Sharma <37150991+wjayesh@users.noreply.github.com>
2020-08-14 22:45:38 +0530

    Create SubArrayOddSum_M.java

---
+++ August/LCC/SubArrayOddSum_M.java
@@ -0,0 +1,17 @@
+
+class SubArrayOddSum_M {
+  public int numOfSubarrays(int[] arr) {
+    int odd = 0, even = 0, sum = 0;
+    for (int n : arr) {
+        if (n % 2 == 1) {
+            int temp = odd;
+            odd = even + 1;
+            even = temp;
+        }
+        else
+            ++even;
+        sum = (sum + odd) % 1000000007;
+    }
+    return sum;
+}
+}
