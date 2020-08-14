8095b397d8116a3d06ab9825eeefa33a631e19da
Jayesh Sharma <37150991+wjayesh@users.noreply.github.com>
2020-08-14 22:48:39 +0530

    dp; single variable.

--- August/LCC/SubArrayOddSum_M.java
+++ August/LCC/SubArrayOddSum_M.java
@@ -13,5 +13,14 @@ class SubArrayOddSum_M {
         sum = (sum + odd) % 1000000007;
     }
     return sum;
+  }
 }
-}
+
+/**
+* If we know the number of even and odd subarrays that end at the previous element, we can figure out how many even and odd subarrays we have for element n:
+
+If n is even, we increase the number of even subarrays; the number of odd subarrays does not change.
+If n is odd, the number of odd subarrays is the previous number of even subarrays + 1. The number of even subarrays is the previous number of odd subarrays.
+**/
+
+
